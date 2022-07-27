33d14a4e2d247bce2846037bf89259100b5cf431
sselvag <swaathy.selva@gmail.com>
2022-07-27 11:52:52 -0400

    image styling

--- src/components/projects.js
+++ src/components/projects.js
@@ -4,14 +4,14 @@ function ProjectCards(props) {
     return (
         <div className="card">
             <div className="img-container">
-                <img alt={props.name} src={props.image}/>
+            <img class="projectImage" alt={props.name} src={props.image}/>
             </div>
 
             <div className="cardContent">
                 <p className="projectTitle" id="projectTitle">{props.name}</p>
                 <p className="projectInfo" id="projectInfo">
                     <a href={props.deploy}><img src="" alt="deployed site" id="project-link"></img></a>
-                    <a href={props.repository}><img src="" alt="deployed site" id="project-link"></img></a>
+                    <a href={props.repository}><img src="" alt="project repo" id="project-link"></img></a>
                 </p>
                 <p className="technologies">
                     ({props.technologies})
