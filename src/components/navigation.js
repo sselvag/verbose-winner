b7478c4468abd0c61936fe68391bc1d309a253cd
sselvag <swaathy.selva@gmail.com>
2022-07-27 11:40:31 -0400

    replaced images

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -11,6 +11,7 @@ function Navigation(props) {
     <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
       <div class="navbar-nav">
         <a class="nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+        <NavLink to="/about"><a class="nav-link" href="#">About Me</a></NavLink>
         <NavLink to="/portfolio"><a class="nav-link">Portfolio</a></NavLink>
         <a class="nav-link" href="https://github.com/sselvag">GitHub</a>
         <a class="nav-link" href="https://ca.linkedin.com/">LinkedIn</a>
